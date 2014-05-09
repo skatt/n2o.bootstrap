2c5dc6e7d3da090557817dc6302b0420fefc9b12
Maxim Sokhatsky <maxim@synrc.com>
2014-05-09 13:25:53 +0300

    WS_SEND

--- include/wf.hrl
+++ include/wf.hrl
@@ -1,4 +1,16 @@
 -ifndef(N2O_BOOTSTRAP_HRL).
 -define(N2O_BOOTSTRAP_HRL, true).
 
+% emulate msg ! socket through wire
+-define(WS_SEND(Id,Ev,Detail), wf:wire(wf:f("document.getElementById('~s').dispatchEvent("
+  "new CustomEvent('~s', {'detail': ~s}));", [Id,wf:to_list(Ev),wf:json([Detail])]))).
+
+% REST macros
+-define(rest(), is_rest() -> true).
+-define(unmap(Record), unmap(P,R) -> wf_utils:hunmap(P,R,record_info(fields, Record),size(R)-1)).
+-define(map(Record), map(O) ->
+    Y = [ try N=lists:nth(1,B), if is_number(N) -> wf:to_binary(B); true -> B end catch _:_ -> B end
+          || B <- tl(tuple_to_list(O)) ],
+    lists:zip(record_info(fields, Record), Y)).
+
 -endif.
